eb3cbfe6fb39b6a7cc61d005d5748f521ed78f2f
Ry Currier <rygc1999@gmail.com>
2025-02-24 22:12:18 -0500

    vst will only compile if the plugin library compilation succeeds, on windows

--- src/build.bat
+++ src/build.bat
@@ -18,6 +18,11 @@ REM compile plugin and host
 REM /PDB:plugin_%date:~-4,4%%date:~-10,2%%date:~-7,2%_%time:~0,2%%time:~3,2%%time:~6,2%%time:~9,2%.pdb
 cl %CFLAGS% ..\src\plugin.cpp -Fmplugin.map -LD /link %LFLAGS% -PDB:plugin_%random%.pdb -EXPORT:renderNewFrame -EXPORT:audioProcess
 
+set PLUGIN_STATUS=%ERRORLEVEL%
+
 cl %CFLAGS% -D"PLUGIN_PATH=\"%cd:\=\\%\\plugin.dll\"" ..\src\main.cpp -Fmmain.map /link %LFLAGS% -LIBPATH:..\src\libs user32.lib gdi32.lib shell32.lib glfw3_mt.lib opengl32.lib miniaudio.lib onnxruntime.lib
 
 popd
+
+exit /b %PLUGIN_STATUS%
+
--- src/build_juce.bat
+++ src/build_juce.bat
@@ -1,6 +1,12 @@
 call "build.bat"
 
-cmake -S . -B ../build/build_JUCE
-::-DJUCE_BUILD_EXTRAS=ON
-cmake --build ../build/build_JUCE
-::--verbose --target AudioPluginHost
+if %ERRORLEVEL% equ 0 (
+  echo plugin build succeeded
+  cmake -S . -B ../build/build_JUCE
+  ::-DJUCE_BUILD_EXTRAS=ON
+  cmake --build ../build/build_JUCE
+  ::--verbose --target AudioPluginHost
+  echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+) else (
+  echo ERROR: plugin build failed. skipping vst compilation  
+)
